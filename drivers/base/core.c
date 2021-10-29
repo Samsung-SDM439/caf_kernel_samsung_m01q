92fd1111b37f489d867df2689e09c3a9103aa723
ShivamKumarJha <jha.shivam3@gmail.com>
2021-10-29 17:23:14 +0000

    Add drivers/base/ modifications
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- drivers/base/core.c
+++ drivers/base/core.c
@@ -868,7 +868,9 @@ static void cleanup_glue_dir(struct device *dev, struct kobject *glue_dir)
 		return;
 
 	mutex_lock(&gdp_mutex);
+	/* HS60 modify for HS60-1138 import patch solve kobject bug by xudayi at 2019/09/17 start */
 	if (!kobject_has_children(glue_dir) && atomic_read(&glue_dir->kref.refcount) == 1)
+	/* HS60 modify for HS60-1138 import patch solve kobject bug by xudayi at 2019/09/17 end */
 		kobject_del(glue_dir);
 	kobject_put(glue_dir);
 	mutex_unlock(&gdp_mutex);
